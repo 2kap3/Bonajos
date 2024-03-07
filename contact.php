7c79f26662651a86773a9b18c11df5ed4758d738
2kap3 <152491355+2kap3@users.noreply.github.com>
2024-03-07 18:19:47 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Profile</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="static.css">
+    <style>
+        html {
+            scroll-behavior: smooth;
+        }
+        /* Center align the content of the section */
+        section {
+            text-align: center;
+        }
+        /* Center align the contact heading and phone number */
+        #contact-info {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <!-- Navigation content -->
+    </nav>
+    <section>
+        <div class="banner">
+            <div class="navbar">
+                <a href="main.html"><img src="" class="logo"></a>
+                <ul>
+                    <li><a href="main.php">Home</a></li>
+                    <li><a href="contact.php">Contact Information</a></li>
+                    <li><a href="portfolio.php">Portfolio</a></li>
+                    <li><a href="resume.php">resume</a></li>
+                    <li><a href="Logout.php">Logout</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <br><br><br><br><br>
+                <img src="boy.png" class="logo-main">
+                <!-- Move the contact section here -->
+                <section id="contact">
+                    <div id="contact-info">
+                        <h2>Contact</h2>
+                        <p>Get in touch with me:</p>
+                        <img src="pon.png" alt="Phone Logo" width="20" height="20">
+                        <span>+639-161-281-635</span>
+                    </div>
+                    <div>
+                        <p>
+                            <a href="https://www.facebook.com/richard.bonajos.12" target="_blank">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/30px-Facebook_Logo_%282019%29.png" alt="Facebook Logo">
+                                Richard Bonajos
+                            </a>
+                        </p>
+                        <p>
+                            <a href="https://www.instagram.com/burdagilll/" target="_blank">
+                                <img src="ss.png" alt="Instagram Logo" width="30" height="30">
+                                burdagilll
+                            </a>
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="footer-col">
+            <div class="logo-container">
+                <img src="11.jpg" alt="Your Logo">
+            </div>
+        </div>
+        <div class="footer-col">
+            <h4>FAVOURITE MOVIES</h4>
+            <ul>
+                <li class="mini-gallery"><a href="#"><img src="4.webp" alt="Team 1"></a></li>
+                <li class="mini-gallery"><a href="#"><img src="5.jpg" alt="Team 2"></a></li>
+                <li class="mini-gallery"><a href="#"><img src="5.jpg" alt="Team 3"></a></li>
+                <li class="mini-gallery"><a href="#"><img src="7.jpg" alt="Team 3"></a></li>
+            </ul>
+        </div>        
+        <div class="footer-col">
+            <h4>Others</h4>
+            <ul class="aye">
+                <li><a href="https://www.y8.com/">
+                    <p>
+                        Play y8 here.
+                    </p>
+                </a></li>
+                <li><a href="https://discord.com/channels/1081889164905164820/1083406319266058270">
+                    <p>
+                        Join our Discord Channel
+                    </p>
+                </a></li>
+                <li><a href="https://www.lazada.com.ph/">
+                    <p>
+                       Shopping ka muna.
+                    </p>
+                </a></li>
+            </ul>
+        </div>
+        <div class="footer-col">
+            <h4>Socials</h4>
+            <div class="links">
+                <a href="https://www.facebook.com/richard.bonajos.12"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/burdagilll/"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
